cfd41ebd007d8ee00da9b6a937590a111450e3f0
Mallikarjuna-dev <76250603+Mallikarjuna-dev@users.noreply.github.com>
2025-01-10 17:09:45 +0530

    server API updated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,7 +7,7 @@ const Home = ({ setUser }) => {
 
     const fetchUser = async () => {
         try {
-            const response = await axios.post('http://localhost:5000/api/user', { username });
+            const response = await axios.post('https://autonomize-task.onrender.com/api/user', { username });
             setUserInfo(response.data);
             setUser(response.data);
         } catch (err) {
